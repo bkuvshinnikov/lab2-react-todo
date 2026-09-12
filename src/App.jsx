d9e865772594f8682cb3b8275a86040779fc94ba
bkuvshinnikov <bkuvshinnikov@users.noreply.github.com>
2026-09-12 10:53:36 +0300

    Simplest app realization.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
+import { useState } from 'react'
+
 function App() {
+  const [task, setTask] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const addTodo = () => {
+    if (task.trim() === '') {
+      return
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: task,
+    }
+
+    setTodos([...todos, newTodo])
+    setTask('')
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
     <div>
       <h1>React Todo App</h1>
+
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+      />
+
+      <button onClick={addTodo}>
+        Add
+      </button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+
+            <button onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
